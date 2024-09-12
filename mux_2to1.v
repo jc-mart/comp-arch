d9a33edc6c6eec797a71a4e43ef714cec63e71ce
Juan C Martinez Jr <83242071+jc-mart@users.noreply.github.com>
2024-09-12 11:04:50 -0500

    Update mux_2to1.v

--- mux_2to1.v
+++ mux_2to1.v
@@ -8,7 +8,7 @@ module mux_2to1(A, B, S_0, Z);
 	wire AonS_0, BaS_0;
 	
 	// Intermediate logic
-	assign AonS_0 = A | !S_0;
+	assign AonS_0 = A | ~S_0;
 	assign BaS_0 = B & S_0;
 	// Output logic
 	assign Z = AonS_0 | BaS_0;
